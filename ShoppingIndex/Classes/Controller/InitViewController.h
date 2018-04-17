9d6c42f35d1ed68ae2180dff43c7f78127cb84a3
朱嘉磊 <jial.zhu@htd.cn>
2018-04-17 15:33:23 +0800

    ShoppingIndex.podspec

---
+++ ShoppingIndex/Classes/Controller/InitViewController.h
@@ -0,0 +1,13 @@
+//
+//  InitViewController.h
+//  HuiLin
+//
+//  Created by liangxiechao on 15/10/8.
+//  Copyright © 2015年 liangxiechao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InitViewController : UIViewController
+
+@end
---
+++ ShoppingIndex/Classes/Controller/InitViewController.m
@@ -0,0 +1,32 @@
+//
+//  InitViewController.m
+//  HuiLin
+//
+//  Created by liangxiechao on 15/10/8.
+//  Copyright © 2015年 liangxiechao. All rights reserved.
+//
+
+#import "InitViewController.h"
+
+@interface InitViewController ()
+
+@end
+
+@implementation InitViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
